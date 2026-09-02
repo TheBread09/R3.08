35fae431671804be3e67bc4f722b0dd1e9e9704b
e2503029 <maxence.nordmann@gmail.com>
2026-09-02 11:42:45 +0200

    TD 1 3.08

---
+++ Exo 1.2.py
@@ -0,0 +1,50 @@
+"""
+def chnombre (ch1 : int, ch2: int) -> int:
+    print ("Veuillez rentre un nombre ")
+    ch1 = int (input ("Nombre: "))
+    ch2 = int (input ("Nombre: "))
+    if ch1 > ch2:
+        return ch1
+    else:
+        return ch2
+print(chnombre (1, 2))
+
+
+
+
+def seuilnombre (ch1 : int, seuil : int) -> int:
+
+    if ch1 > seuil:
+        return print('AU DESSSUS')
+    else:
+        return print("dessous")
+
+ch1 = int (input ("Nombre: "))
+print (seuilnombre (1,10))
+
+
+
+
+def Gnombre(*nombre):
+  maxN = nombre[0]
+  for i in nombre:
+    if i > maxN:
+      maxN = i
+  return maxN
+
+print(Gnombre(3, 7, 2, 9, 1, 18, 7))
+
+
+
+def mini(*nombre)-> int:
+    b = 3
+    total = 0
+    for i in nombre:
+        if i < b :
+            total += i
+    return total
+print(mini(1, 3, 5,4,5,8,9,1,2,0,2,1,4,8,6))
+
+
+"""
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # R3.08
 Cours R3.08
+
+POUR ENVIRONNEMENT VIRTUEL
+python -m venv venv
+./.venv/Scripts/activate
